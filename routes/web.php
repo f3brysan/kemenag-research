afa6d41ce869cf2bbf58706564fdc68ff29122fa
Febry San <97709170+f3brysan@users.noreply.github.com>
2024-12-23 13:58:28 +0700

    ✨ feat: Add MapsController and maps view with routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MapsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/maps', [MapsController::class, 'index']);
